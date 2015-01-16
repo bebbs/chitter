cb490f099ec18cfbff20feb5c60b9f32b4a48e7e
Josh Bebbington <josh@focalwebsites.com>
2015-01-16 16:11:32 +0000

    Only logged in users can post peeps

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,29 +2,35 @@ require 'spec_helper'
 require 'helpers/user_management.rb'
 
 feature 'Posting a peep' do
-
-  before(:each) do
-    sign_up
-    sign_in('josh@test.com', 'test')
-    expect(page).to have_content('Welcome, Josh')
-  end
   
   scenario 'A user has no peeps when they first sign up' do
+    sign_up_and_in
     user = User.first
     expect(user.peeps.length).to eq 0
   end
 
   scenario 'A user can post a peep' do
-    user = User.first
+    sign_up_and_in
     post_peep
+    user = User.first
     expect(Peep.first.content).to eq 'Test first peep'
     expect(Peep.first.user_id).to eq user.id
   end
 
+  scenario "Logged in users can't post peeps" do
+    visit '/'
+    expect(page).not_to have_content('Post a peep:')
+  end
 end
 
 def post_peep
   visit '/'
   fill_in 'peep', with: 'Test first peep'
   click_button 'Post'
+end
+
+def sign_up_and_in
+  sign_up
+  sign_in('josh@test.com', 'test')
+  expect(page).to have_content('Welcome, Josh')
 end
